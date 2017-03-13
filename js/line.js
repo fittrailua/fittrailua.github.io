25ea1a4412bb7d2c19c957f9886068ed4f4b12b6
Nataliia Kumeiko <nkumeiko@gmail.com>
2017-03-13 22:58:29 +0200

    TMP: animate path

--- js/line.js
+++ js/line.js
@@ -1,13 +1,26 @@
 $(function() {
+    // var path = document.getElementById("path");
+    // var dashOffset = parseInt($("#path").css("stroke-dashoffset"), 10);
+
+    // $(window).scroll(function() {
+    //     var percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
+    //     var offsetUnit = (percentageComplete * (dashOffset / 100));
+    //     $("#path").css("stroke-dashoffset", dashOffset - offsetUnit);
+    // });
+
+
+
     var path = document.getElementById("path");
     var length = path.getTotalLength();
 
+    path.style.strokeDasharray = length;
     path.style.strokeDashoffset = length;
 
     function updatePath() {
-      var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-      var draw = length * scrollpercent;
-      path.style.strokeDashoffset = length - draw;
+        var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+        var draw = length * scrollpercent;
+        path.style.strokeDashoffset = length - draw;
     }
 
     window.addEventListener("scroll", updatePath);
